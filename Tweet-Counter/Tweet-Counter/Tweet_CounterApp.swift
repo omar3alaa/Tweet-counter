ad5c1075bafb03206ae77da99304a961058877d7
omar3alaa <omar.alaa.fcih@gmail.com>
2022-10-14 21:38:34 +0200

    Added project

---
+++ Tweet-Counter/Tweet-Counter/Tweet_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tweet_CounterApp.swift
+//  Tweet-Counter
+//
+//  Created by Omar Alaa on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tweet_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
